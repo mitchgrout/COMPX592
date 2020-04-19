13acdabc7c4e8791678516bb1f646292bdfe8a05
Mitchell Grout <mitchell.grout@hotmail.com>
2020-04-19 19:16:18 +1200

    Overhaul neat wrapper

--- ne/neat.py
+++ ne/neat.py
@@ -5,9 +5,88 @@ ne.neat
 
 from ne.base        import Model
 from ne.execute     import Sequential
-#from ne.visualize   import draw_net
 from neat.reporting import BaseReporter
 from ne.util        import benchmark
+
+def create_config_file(**kwargs):
+    import tempfile
+    # neat has a bad API for doing this, so we're going to patch it
+    default_config = {
+        'NEAT': {
+            'fitness_criterion':'max',
+            'fitness_threshold': 1.1,
+            'pop_size': 128,
+            'reset_on_extinction': False,
+        },
+
+        'DefaultGenome': {
+            'activation_default': 'identity',
+            'activation_mutate_rate': 0.2,
+            'activation_options': 'identity relu clamped tanh sigmoid',
+            'aggregation_default': 'sum',
+            'aggregation_mutate_rate': 0.2,
+            'aggregation_options': 'sum',
+            'bias_init_mean': 0.0,
+            'bias_init_stdev': 1.0,
+            'bias_max_value': 30.0,
+            'bias_min_value': -30.0,
+            'bias_mutate_power': 0.5,
+            'bias_mutate_rate': 0.7,
+            'bias_replace_rate': 0.1,
+            'compatibility_disjoint_coefficient': 1.0,
+            'compatibility_weight_coefficient': 0.5,
+            'conn_add_prob': 0.5,
+            'conn_delete_prob': 0.5,
+            'enabled_default': True,
+            'enabled_mutate_rate': 0.01,
+            'feed_forward': True,
+            'initial_connection': 'full_direct',
+            'node_add_prob': 0.2,
+            'node_delete_prob': 0.5,
+            'num_hidden': 0,
+            'num_inputs': 0,
+            'num_outputs': 1,
+            'response_init_mean': 1.0,
+            'response_init_stdev': 0.0,
+            'response_max_value': 30.0,
+            'response_min_value': -30.0,
+            'response_mutate_power': 0.0,
+            'response_mutate_rate': 0.0,
+            'response_replace_rate': 0.0,
+            'weight_init_mean': 0.0,
+            'weight_init_stdev': 1.0,
+            'weight_max_value': 30.0,
+            'weight_min_value': -30.0,
+            'weight_mutate_power': 0.5,
+            'weight_mutate_rate': 0.8,
+            'weight_replace_rate': 0.1,
+        },
+
+        'DefaultSpeciesSet': {
+            'compatibility_threshold': 3.0, 
+        },
+
+        'DefaultStagnation': {
+            'species_fitness_func': 'max',
+            'max_stagnation': 20,
+            'species_elitism': 2,
+        },
+
+        'DefaultReproduction': {
+            'elitism': 2,
+            'survival_threshold': 0.2,
+        },
+    }
+
+    config_file = tempfile.mktemp()
+    with open(config_file, 'w') as fd:
+        for section in default_config.keys():
+            fd.write('[{}]\n'.format(section))
+            for (key, value) in default_config[section].items():
+                if key in kwargs:
+                    value = kwargs[key]
+                fd.write('{} = {}\n'.format(key, value))
+    return config_file
 
 def _render_neat_model(base_model, label_names, filename):
     import graphviz
@@ -75,12 +154,15 @@ class Recurrent(Model):
 
 # Hacky, but I guess it works
 epoch = 0
-def _batch(train, num_splits, model_type, fitness, thresh, executor, genomes, config):
+def _batch(train, num_chunks, model_type, fitness, thresh, executor, genomes, config):
+    from math import ceil
     global epoch
+    l        = ceil(train.xs.shape[0] / num_chunks)
+    offset   = (epoch % num_chunks) * l
     epoch   += 1
-    l        = len(train.xs) / num_splits
-    train_xs = train.xs#[int(l*(epoch-1)):int(l*epoch)] # NOTE: Small segs instead of growing in size
-    train_ys = train.ys#[int(l*(epoch-1)):int(l*epoch)] # NOTE: 
+    
+    train_xs = train.xs[offset : offset + l]
+    train_ys = train.ys[offset : offset + l]
     
     def flatten(xss): return [x for xs in xss for x in xs]
     def _inner(s_genomes):
@@ -102,71 +184,48 @@ def _batch(train, num_splits, model_type, fitness, thresh, executor, genomes, co
 
 
 class StatsReporter(BaseReporter):
-    def __init__(self, split_data, model_type, fitness, thresh):
-        self.split_data = split_data
-        self.model_type = model_type
-        self.fitness    = fitness
-        self.thresh     = thresh
+    def __init__(self, split_data, model_type, fitness, thresh, verbose):
+        self.split_data   = split_data
+        self.model_type   = model_type
+        self.fitness      = fitness
+        self.thresh       = thresh
+        self.verbose      = verbose
+        self.best_model   = None
+        self.best_fitness = None
 
     def post_evaluate(self, config, _0, _1, best_genome):
         m    = self.model_type(self.fitness, self.thresh, best_genome, config)
-        #train= self.split_data.train
         val  = self.split_data.val
-        test = self.split_data.test
-
         val_pred_ys  = list(m.predict(val.xs))
-        test_pred_ys = list(m.predict(test.xs))
-
-         #*_, train_fitness = m.evaluate(train.ys, m.predict(train.xs))
         *_, val_fitness  = m.evaluate(val.ys,  val_pred_ys)
-        *_, test_fitness = m.evaluate(test.ys, test_pred_ys)
 
-        # print("Train fitness", train_fitness)
-        print("Validation fitness:", val_fitness)
-        print("Test fitness      :", test_fitness)
+        if self.best_fitness is None or val_fitness > self.best_fitness:
+            self.best_model   = m
+            self.best_fitness = val_fitness
 
-        # print("Train      :", benchmark(lambda:m.compute_statistics(train.ys,m.predict(train.xs))))
-        print("Validation stats:", m.compute_statistics(val.ys , val_pred_ys ))
-        print("Test stats      :", m.compute_statistics(test.ys, test_pred_ys))
+        if self.verbose:
+            print("Validation fitness:", val_fitness)
+            # print("Validation stats:  ", stats)
        
+def run(epochs, split_data, num_chunks, model_type, fitness, config_file, log_dir,
+        executor=Sequential(), thresh=lambda x: x>0.5, verbose=True):
 
-class ModelSaver(BaseReporter):
-    def __init__(self, log_dir, model_type, fitness, thresh):
-        import os
-        self.model_type = model_type
-        self.fitness    = fitness
-        self.thresh     = thresh
-        self.dir        = '{}/models'.format(log_dir)
-        os.makedirs(self.dir, exist_ok=True)
-        
-    def start_generation(self, gen):
-        self.gen = gen
-
-    def post_evaluate(self, config, _0, _1, best_genome):
-        m = self.model_type(self.fitness, self.thresh, best_genome, config)
-        m.save('{}/{}.pkl'.format(self.dir, self.gen))
-
-
-def run(epochs, split_data, model_type, fitness, config_file, log_dir,
-        executor=Sequential(), thresh=lambda x: x>0.5, train_ratio=0.8,
-        num_splits=None):
-    """
-    """
-    
     import neat
     from functools import partial
+    global epoch
+    epoch = 0 # For re-running [if we ever do that]
 
-    if num_splits == None: num_splits = epochs
-    assert num_splits != None
-
-    evaluator = partial(_batch, split_data.train, num_splits, model_type, fitness, thresh, executor) 
+    evaluator = partial(_batch, split_data.train, num_chunks, model_type, fitness, thresh, executor) 
     config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
                          neat.DefaultSpeciesSet, neat.DefaultStagnation,
                          config_file)
     population = neat.Population(config)
-    population.add_reporter(neat.StdOutReporter(False))
-    population.add_reporter(StatsReporter(split_data, model_type, fitness, thresh))
-    population.add_reporter(ModelSaver(log_dir, model_type, fitness, thresh))
+    if verbose:
+        population.add_reporter(neat.StdOutReporter(False))
+    reporter = StatsReporter(split_data, model_type, fitness, thresh, verbose)
+    population.add_reporter(reporter)
   
-    winner = population.run(evaluator, epochs)
+    # Overall winner != best training fitness
+    population.run(evaluator, epochs)
+    return reporter.best_model
 
